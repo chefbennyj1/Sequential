4882c833593ea659b44f54ae6a19a334be83119c
Benjamin Anderson <chefbennyj@gmail.com>
2026-06-07 15:22:14 -0400

    Dramatically slow landing page reveal: increased duration to 6000ms for a more atmospheric and complete visual transition

--- views/landing/landingPage.js
+++ views/landing/landingPage.js
@@ -113,9 +113,9 @@ async function showNextImage() {
     _heroImage.src = imageUrl;
     
     if (container) {
-        await imageMaskReveal([container], gifUrl, 4000);
+        await imageMaskReveal([container], gifUrl, 6000);
     } else {
-        await imageMaskReveal([_heroImage.parentElement], gifUrl, 4000);
+        await imageMaskReveal([_heroImage.parentElement], gifUrl, 6000);
     }
     
     // Update index only after successful transition
